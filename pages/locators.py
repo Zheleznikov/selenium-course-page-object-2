d322c12d325ab1f5d2b932183d256be7f4d04d7e
Serg Zheleznikov <polwen@yandex.ru>
2020-12-17 16:40:04 +0300

    add pages

---
+++ pages/locators.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+class Urls():
+    MAIN_PAGE_URL = 'http://selenium1py.pythonanywhere.com/'
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
+
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
+    REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
