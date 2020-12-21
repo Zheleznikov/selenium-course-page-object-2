da4be5bc63d02cc62654b8e15d13803efe194abd
Serg Zheleznikov <polwen@yandex.ru>
2020-12-21 15:16:52 +0300

    delete comments

--- pages/locators.py
+++ pages/locators.py
@@ -18,22 +18,22 @@ class Urls:
     ]
 
 
-class BasePageLocators:
+class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LOCATOR = (By.CSS_SELECTOR, ".basket-mini > span > .btn")
 
 
-class MainPageLocators:
+class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
-class LoginPageLocators:
+class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 
-class ProductPageLocators:
+class ProductPageLocators():
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
     PRODUCT_PRICE_LOCATOR = (By.CSS_SELECTOR, ".product_main > .price_color")
     PRODUCT_NAME_LOCATOR = (By.CSS_SELECTOR, ".product_main > h1")
@@ -41,7 +41,7 @@ class ProductPageLocators:
     ALERT_WITH_BASKET_PRICE = (By.CSS_SELECTOR, "#messages > .alert", 2)
     BASKET_PRODUCT_NAME_LOCATOR = (By.CSS_SELECTOR, ".alertinner > strong")
     BASKET_PRODUCT_PRICE_LOCATOR = (By.CSS_SELECTOR, ".alertinner > p > strong")
-    
+
 
 class BasketPageLocators():
     PRODUCT_IN_BASKET_FORM = (By.CSS_SELECTOR, "#basket_formset")
